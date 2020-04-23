31509fea789980f097f9dc1c465c93e1cc88724b
heikki siikavirta <heikki.siikavirta@kidesystems.com>
2020-04-23 09:00:12 +0300

    JAVA 122 proceeded

--- Java/Java Programming Masterclass for Software Developers/src/tutorial_80_inheritance/Main.java
+++ Java/Java Programming Masterclass for Software Developers/src/tutorial_80_inheritance/Main.java
@@ -3,7 +3,11 @@ package tutorial_80_inheritance;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Initial commit");
+		Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+		Dog dog = new Dog("YorkiE", 8, 20, 2, 4, 1, 20, "Long silky");
+		dog.eat();
+
 	}
 
 }
